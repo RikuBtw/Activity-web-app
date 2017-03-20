d16f061e840599fdb268e40925489671a2e16567
e155024e <e155024e@etu.univ-nantes.fr>
2017-03-20 14:25:49 +0100

    commit4

--- sgbd.py
+++ sgbd.py
@@ -8,6 +8,8 @@ class SGBD:
         self.cnx = mysql.connector.connect(user='E154817E', password = 'E154817E', database='E154817E', host ='infoweb')
         self.cursor = self.cnx.cursor()
 
+
+    # renvoie toutes les villes et les activités disponibles
     def datalist(self):
         query = ("SELECT ComLib, ActLib, ActNivLib FROM activite")
         self.cursor.execute(query)
@@ -19,33 +21,42 @@ class SGBD:
             rData+= str(data)
         return rData
 
+    # à partir du nom d'une ville, renvoie toutes les activités et leurs niveaux disponibles dans la ville
     def ville_act(self,ville):
         query = ("SELECT ActLib, ActNivLib FROM activite WHERE ComLib=%s")
         ville = (ville,)
-        rActivite = None
+        rActivite = []
         self.cursor.execute(query, ville)
         for (a) in self.cursor:
-            rActivite={a[0], a[1]}
+            paire = { a[0], a[1] }
+            rActivite.append( paire )
 
         return rActivite
 
+    # à partir d'une activité, renvoie toutes les villes et leurs codes postaux qui permettent cette activité
     def act_ville(self,act):
         act.capitalize()
         query = ("SELECT ComLib, ComInsee FROM activite WHERE ActLib=%s")
         act = (act,)
-        rVille = None
+        rVille = []
         self.cursor.execute(query, act)
         for (v) in self.cursor:
-            rVille={v[0], v[1]}
+            paire ={v[0], v[1]}
+            rVille.append( paire )
         return rVille
 
-    def niveau(self,niv):
+    # à partir d'une activité et son niveau, renvoie toutes les villes disponible
+    def niveau(self,act,niv):
         niv.capitalize()
-        query = ("SELECT ActLib FROM activite WHERE ActNivLib=%s")
-        niv = (niv,)
-        rNiveau = None
-        self.cursor.execute(query, niv)
+        query = ("SELECT ComLib, ComInsee FROM activite WHERE ActLib=%s AND ActNivLib=%s")
+        actNiv = (act,niv)
+        rNiveau = []
+        self.cursor.execute(query, actNiv)
         for (a) in self.cursor:
-          rNiveau={a[0], a[1]}
+          paire = {a[0], a[1]}
+          rNiveau.append( paire )
         self.cursor.close()
         return rNiveau
+
+s = SGBD()
+print(s.niveau("Basket-Ball","Compétition national"))
