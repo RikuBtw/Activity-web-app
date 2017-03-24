b4e7d2a20714acade37f730f24877cd8fb5162a6
e155024e <e155024e@etu.univ-nantes.fr>
2017-03-24 14:13:52 +0100

    base de donnees

--- sgbd.py
+++ sgbd.py
@@ -7,6 +7,8 @@ class SGBD:
     def __init__(self):
         self.cnx = mysql.connector.connect(user='E154817E', password = 'E154817E', database='E154817E', host ='infoweb')
         self.cursor = self.cnx.cursor()
+        self.latitude = self.cnx.cursor()
+        self.longitude = self.cnx.cursor()
 
     #renvoie toutes les villes
     def villes(self):
@@ -50,17 +52,17 @@ class SGBD:
     # à partir d'une activité et d'un niveau, renvoie toutes les villes qui permettent cette activité
     def act_ville(self,act,niv):
         act.capitalize()
+        villes = []
+        tmp = []
         query = ("SELECT ComLib FROM activite WHERE ActLib=%s AND ActNivLib=%s GROUP BY ComLib")
         paire = (act,niv)
         rVille = []
         self.cursor.execute(query, paire)
         for (a) in self.cursor:
-            res = []
-            print(a[0])
-            lat = self.LatitudeGPS(a[0])
-            long = self.LongitudeGPS(a[0])
-            res = a[0]
-            rVille.append(res)
+            villes.append(a[0])
+        for ville in villes:
+            tmp = ville, self.PositionGPS(ville)
+            rVille.append(tmp)
         return rVille
 
     # à partir d'une ville, d'une activité et d'un niveau, renvoie tous ses équipements
@@ -76,33 +78,26 @@ class SGBD:
             listeEquId.append(a[0])
         for (a) in listeEquId:
             paire = (a,)
-            query = ("SELECT EquNom FROM equipement WHERE EquipementId=%s")
+            query = ("SELECT EquNom, EquGpsX, EquGpsY FROM equipement WHERE EquipementId=%s")
             self.cursor.execute(query, paire)
             for (a) in self.cursor:
-                rRecherche.append(a[0])
+                tmp = a[1],a[2]
+                tmp2= a[0],tmp
+                rRecherche.append(tmp2)
         return rRecherche
 
-    # à partir d'une ville, renvoie sa latitude
-    def LatitudeGPS(self, ville):
+    # à partir d'une ville, renvoie sa latitude et sa longitude
+    def PositionGPS(self, ville):
         ville.capitalize()
-        query = ("SELECT Latitude FROM commune WHERE ComLib=%s GROUP BY Latitude")
+        query = ("SELECT Latitude, Longitude FROM commune WHERE ComLib=%s GROUP BY Latitude")
         ville = (ville,)
         rVille = []
-        self.cursor.execute(query, ville)
-        for (a) in self.cursor:
-          rVille = a[0]
+        self.latitude.execute(query, ville)
+        for (a) in self.latitude:
+          rVille = a[0],a[1]
         return rVille
 
-    # à partir d'une ville, renvoie sa longitude
-    def LongitudeGPS(self, ville):
-        ville.capitalize()
-        query = ("SELECT Longitude FROM commune WHERE ComLib=%s GROUP BY Longitude")
-        ville = (ville,)
-        rVille = []
-        self.cursor.execute(query, ville)
-        for (a) in self.cursor:
-          rVille = a[0]
-        return rVille
 
 bd = SGBD()
-tmp = bd.act_ville("Basket-Ball", "Scolaire")
+tmp = bd.equipements_villes("Nantes","Basket-Ball", "Scolaire")
+print(tmp)
